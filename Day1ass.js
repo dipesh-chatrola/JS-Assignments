4ca14bc53daae3c0707a20f2bef1e25ca33bdd99
dipesh-chatrola <87219133+dipesh-chatrola@users.noreply.github.com>
2021-08-04 15:47:57 +0530

    JS_Day 1 Assignment Submitted
    Task: Create two variables and perform all arithmetic (addition, subtraction..etc ) operations and print the result in the console using templating syntax.

---
+++ Day1ass.js
@@ -0,0 +1,19 @@
+// Name: Dipesh Chatrola
+//Day1 Assignment
+//Create two variables and perform all arithmetic (addition, subtraction..etc ) operations and print the result in the console using templating syntax.
+
+
+
+let a = 20;
+let b = 5;
+
+let Add  = `The total is ${a + b}`;
+console.log(Add);
+let Sub  = `The total is ${a - b}`;
+console.log(Sub);
+let Mul  = `The total is ${a * b}`;
+console.log(Mul);
+let Div  = `The total is ${a / b}`;
+console.log(Div);
+let Mod  = `The total is ${a % b}`;
+console.log(Mod);
---
+++ first.js
@@ -0,0 +1,66 @@
+// Conditional Statements
+//if--> to write a conditional statement
+
+let age=20;
+
+if (age>=18)
+{
+    
+    console.log("Hello");
+
+}
+else 
+{
+    
+    console.log("Sorry");
+
+}
+
+console.log("How are you?");
+
+
+ //if age is 18+ then only execute
+
+
+
+ for (let i=0; i<=10; i++)
+ {
+     console.log(i);
+ }
+
+
+
+ let arr=["Thor","Ironman","Hulk"];
+ console.log(arr);
+ console.log(arr.length);
+ for (let j=1;j<arr.length; j++)
+ {
+     console.log(arr[j],j);
+ }
+
+
+ let k=10;
+ do
+ {
+    console.log(k);
+    k++;
+ }
+ while (k<=10);
+ for(let k=1;k<=10;k++)
+ {
+     console.log(k);
+ }
+
+let a = 20;
+let b = 5;
+
+let Add  = `The total is ${a + b}`;
+console.log(Add);
+let Sub  = `The total is ${a - b}`;
+console.log(Sub);
+let Mul  = `The total is ${a * b}`;
+console.log(Mul);
+let Div  = `The total is ${a / b}`;
+console.log(Div);
+let Mod  = `The total is ${a % b}`;
+console.log(Mod);
